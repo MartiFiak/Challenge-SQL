56f4ce286827023a6762fe3ecf06e283b8e955e9
MartiFiak <jacolotmarine@gmail.com>
2023-10-23 22:42:36 +0200

    Modify total-invoices.sql

--- total-invoices.sql
+++ total-invoices.sql
@@ -1,4 +1,4 @@
-SELECT FirstName || ' ' || UPPER(LastName) AS 'FullName', SUM(invoices.Total)
+SELECT FirstName || ' ' || UPPER(LastName) AS 'FullName', SUM(invoices.Total) AS "AllInvoices"
 FROM customers
 INNER JOIN invoices ON customers.CustomerId = invoices.CustomerId
 GROUP BY invoices.CustomerId
