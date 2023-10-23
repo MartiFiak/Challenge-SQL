628ab93f3274f20a09e76e1673217167aba02e47
MartiFiak <jacolotmarine@gmail.com>
2023-10-23 22:41:07 +0200

    Add  total-invoices.sql

---
+++ total-invoices.sql
@@ -0,0 +1,6 @@
+SELECT FirstName || ' ' || UPPER(LastName) AS 'FullName', SUM(invoices.Total)
+FROM customers
+INNER JOIN invoices ON customers.CustomerId = invoices.CustomerId
+GROUP BY invoices.CustomerId
+HAVING sum(invoices.Total) > 38
+ORDER BY FullName ASC
