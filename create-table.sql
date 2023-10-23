31fd9be4c8840c14c7e7e304c204c2c82a18527b
MartiFiak <jacolotmarine@gmail.com>
2023-10-23 10:19:58 +0200

    modifications create-table.sql

--- create-table.sql
+++ create-table.sql
@@ -3,6 +3,6 @@ CREATE TABLE students
     StudentId INTEGER PRIMARY KEY NOT NULL,
     FirstName VARCHAR(80) NOT NULL,
     LastName VARCHAR(80) NOT NULL,
-    BirthDay DATE NOT NULL,
+    Birthday DATE NOT NULL,
     City VARCHAR(50) NOT NULL
 )
