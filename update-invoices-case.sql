ef5573bdd934850fa43e83fe5abbd9d4f5d0e332
MartiFiak <jacolotmarine@gmail.com>
2023-10-23 23:20:47 +0200

    Modify update-invoices-case.sql

--- update-invoices-case.sql
+++ update-invoices-case.sql
@@ -3,6 +3,6 @@ SET Total = (
     CASE
         WHEN BillingCountry = 'FRANCE' THEN Total + 3
         WHEN BillingCountry = 'USA' THEN Total + 5
-        ELSE Total + 3
+        ELSE Total + 1
     END
     )
