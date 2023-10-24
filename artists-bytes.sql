5cbf2cdebb74838cd7aa3faba65ad293da0420a6
MartiFiak <jacolotmarine@gmail.com>
2023-10-24 11:47:49 +0200

    Add artists-bytes.sql

---
+++ artists-bytes.sql
@@ -0,0 +1,5 @@
+SELECT artists.Name AS 'ArtistName', albums.Title AS 'AlbumName', tracks.Name AS 'TrackName', Round(tracks.Bytes / 1000000.0, 2) || ' MB' AS 'MegaBytes'
+FROM tracks
+INNER JOIN albums ON tracks.AlbumId = albums.AlbumId
+INNER JOIN artists ON albums.ArtistId = artists.ArtistId
+WHERE AlbumName = 'American Idiot'
