f533034024cfc80e04c95e75b072195e009c1d7d
MartiFiak <jacolotmarine@gmail.com>
2023-10-24 15:38:23 +0200

    Add third-best-seller.sql

--- third-best-seller.sql
+++ third-best-seller.sql
@@ -1,7 +1,5 @@
 SELECT employees.LastName || ' ' || employees.FirstName AS '3rd best seller'
-FROM employees
-WHERE EmployeeId = (
-    SELECT EmployeeId
-    FROM employees
-    WHERE EmployeeId = 5
-    )
+FROM employees join main.customers c on employees.EmployeeId = c.SupportRepId
+GROUP BY EmployeeId
+ORDER BY SupportRepId DESC
+LIMIT 1
