24e56bf7080720f4f8470485a815d53a14b0bba5
MartiFiak <jacolotmarine@gmail.com>
2023-10-24 10:28:12 +0200

    Add invoices-items-last.sql

--- invoices-items-last.sql
+++ invoices-items-last.sql
@@ -4,5 +4,7 @@ INNER JOIN tracks ON invoice_items.TrackId = tracks.TrackId
 WHERE 'Name' = (
     SELECT tracks.Name
     FROM tracks
+    INNER JOIN invoices
+    ORDER BY InvoiceDate DESC
     LIMIT 1
           )
