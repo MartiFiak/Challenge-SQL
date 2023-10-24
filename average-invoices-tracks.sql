c431afdc3bede979eee0796fdfcf03d2c800a3de
MartiFiak <jacolotmarine@gmail.com>
2023-10-24 16:49:22 +0200

    Add average-invoices-tracks.sql

--- average-invoices-tracks.sql
+++ average-invoices-tracks.sql
@@ -1,4 +1,4 @@
-SELECT InvoiceId, round(avg(tracks.UnitPrice), 2) AS 'Average Price', floor(sum(Milliseconds*0.001)) AS 'Track Total Time', round(round(avg(tracks.UnitPrice), 2)/floor(sum(Milliseconds*0.001)), 5) || '€' AS 'Price By Second'
+SELECT InvoiceId, round(avg(tracks.UnitPrice), 2) AS 'Average Price', floor(sum(Milliseconds*0.001)) AS 'Track Total Time', round(round(avg(tracks.UnitPrice), 2)/floor(sum(Milliseconds*0.001)), 5) || '€' AS 'Price by second'
 FROM tracks
 INNER JOIN main.invoice_items ii on tracks.TrackId = ii.TrackId
 GROUP BY InvoiceId
