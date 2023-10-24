f617e0a4519c258663de25732b2038beaaa2e999
MartiFiak <jacolotmarine@gmail.com>
2023-10-24 09:13:20 +0200

    Update customers-continent.sql

--- customers-continent.sql
+++ customers-continent.sql
@@ -1,6 +1,8 @@
 SELECT FirstName, LastName, Country,
 CASE
-    WHEN Country = 'Brazil' OR Country = 'Canada' OR Country = 'USA' THEN 'America'
+    WHEN Country = 'Brazil' OR Country = 'Canada' OR Country = 'USA' OR Country = 'Argentina' OR Country = 'Chile' THEN 'America'
+    WHEN Country = 'Australia' THEN 'Oceania'
+    WHEN Country = 'India' THEN 'Asia'
     ELSE 'Europe'
 END AS 'Continent'
 FROM customers
