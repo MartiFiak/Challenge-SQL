8d740ef7f9bcc6bd2686e800bd2d3844d19354df
MartiFiak <jacolotmarine@gmail.com>
2023-11-09 16:18:46 +0100

    Add count-all-country-from-tables.sql

---
+++ advanced-sql/count-all-country-from-tables.sql
@@ -0,0 +1,25 @@
+SELECT Country, COUNT(*) AS "Total",
+
+    SUM(CASE
+        WHEN Source = "E" THEN 1 ELSE 0 END
+    ) AS "Employees",
+
+    SUM(CASE
+        WHEN Source = "C" THEN 1 ELSE 0 END
+    ) AS "Customers",
+
+    SUM(CASE
+        WHEN Source = "I" THEN 1 ELSE 0 END
+    ) AS "Invoices"
+
+FROM (
+         SELECT Country, "E" as Source
+         FROM employees
+         UNION ALL
+         SELECT Country, "C" as Source
+         FROM customers
+         UNION ALL
+         SELECT BillingCountry, "I" as Source
+         FROM invoices
+     )
+GROUP BY Country;
