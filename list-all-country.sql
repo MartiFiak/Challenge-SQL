a6ef258753f6fe5fb61fda73cd56534fefe83f7e
MartiFiak <jacolotmarine@gmail.com>
2023-11-09 11:12:28 +0100

    Add list-all-country.sql

---
+++ list-all-country.sql
@@ -0,0 +1,12 @@
+SELECT Country, COUNT(*) AS "Total"
+FROM (
+    SELECT Country
+    FROM employees
+    UNION ALL
+    SELECT Country
+    FROM customers
+    UNION ALL
+    SELECT BillingCountry
+    FROM invoices
+     )
+GROUP BY Country
